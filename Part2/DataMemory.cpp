c57250b2bf9a5eb494b3b21cf143165f1c56e2d0
niemeyj <niemeyj@purdue.edu>
2022-11-11 14:03:47 -0500

    update data memory with functionality

--- Part2/DataMemory.cpp
+++ Part2/DataMemory.cpp
@@ -1,11 +1,12 @@
 #include "DataMemory.h"
 #include <string>
 #include <vector>
+#include "RunStack.h"
 
 DataMemory* DataMemory::uniqueInstance = nullptr;
  
 DataMemory::DataMemory(){
-    scope_indexes.push_back(0);
+    //scope_indexes.push_back(0);
 };
  
 DataMemory* DataMemory::getInst(){
@@ -15,14 +16,42 @@ DataMemory* DataMemory::getInst(){
     return uniqueInstance;
 }
 
-void DataMemory::addStack(int arr_size){
-    int startNewScope = data_mem.size();
-    scope_indexes.push_back(startNewScope);
-    scope++;
-    
+void DataMemory::addStack(int scope_size){
+
+    for(int i=0; i<scope_size; i++){
+        data_mem.push_back(0); // initialized new scope values with zeros
+
+    }
 
 }
 
-void DataMemory::popStack(int opnd){
-    int dataMemLocation = scope_indexes[scope] + opnd - 1;
+void DataMemory::popScl(int opnd, RunStack* rs){
+    data_mem[opnd] = rs->run_stack[0];
+    rs->run_stack.erase(rs->run_stack.begin());
+}
+
+void DataMemory::popArr(int opnd, RunStack* rs){
+    int stackValue = rs->run_stack[0];
+    int e = opnd + stackValue;
+    rs->run_stack.erase(rs->run_stack.begin());
+   
+    data_mem[e] = stackValue;
+}
+
+void DataMemory::pushScl(int opnd, RunStack* rs){
+
+    rs->run_stack.insert(rs->run_stack.begin(), data_mem[opnd]);
+}
+
+void DataMemory::pushArr(int opnd, RunStack* rs){
+    int e = opnd + rs->run_stack[0];
+    rs->run_stack.erase(rs->run_stack.begin());
+    rs->run_stack.insert(rs->run_stack.begin(), data_mem[e]);
+
+}
+
+void DataMemory::deleteStack(int size){
+    for(int i=0; i<size; i++){
+        data_mem.pop_back();
+    }
 }
--- Part2/DataMemory.h
+++ Part2/DataMemory.h
@@ -2,16 +2,20 @@
 #define DataMemory_h
 #include <string>
 #include <vector>
+#include "RunStack.h"
 
 class DataMemory {
     public:
         static DataMemory* getInst();
         void addStack(int arr_size);
-        void deleteStack();
+        void deleteStack(int arr_size);
         std::vector<int> data_mem;
-        std::vector<int> scope_indexes; //hold index of start of new stack
-        int scope = 0;
-        void popStack(int opnd);
+        //std::vector<int> scope_indexes; //hold index of start of new stack
+        //int scope = 0;
+        void popScl(int opnd, RunStack* rs);
+        void popArr(int opnd, RunStack* rs);
+        void pushScl(int opnd, RunStack* rs);
+        void pushArr(int opnd, RunStack* rs);
 
     private:
         DataMemory();
